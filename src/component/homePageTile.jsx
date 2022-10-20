64ac0f4976c52c1cb33d98365a78b77ab44806b4
Srijan SIngh <srijan@bhyve.io>
2022-10-20 13:04:22 +0530

    responsive fix

--- src/component/homePageTile.jsx
+++ src/component/homePageTile.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 function HomePageTile(props) {
   return (
-    <Box height="132px" w={["330px","400px"]} bg="white" py="10px" px="10px" borderRadius="12px">
+    <Box height="132px" w={["250px","330px","400px"]} bg="white" py="10px" px="10px" borderRadius="12px">
       <HStack alignItems="flex-start">
         <Image boxSize="100px" src={props.imageUrl} />
         <VStack pl="10px" alignItems="flex-start">
-          <Text fontWeight="300" fontSize="18px" mb="-5px">
+          <Text fontWeight="300" fontSize={["13px","18px"]} mb="-5px">
             {props.normalText}
           </Text>
-          <Text fontSize="18px" fontWeight="600">
+          <Text fontSize={["13px","18px"]} fontWeight="600">
             {props.boldText}
           </Text>
         </VStack>
